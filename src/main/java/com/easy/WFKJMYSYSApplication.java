5ee86bae144e1664d9ffe8e126f594a0b1eed6f9
rich <rich@itcast.cn>
2024-12-17 10:17:55 +0800

    wfkj1.2

---
+++ src/main/java/com/easy/WFKJMYSYSApplication.java
@@ -0,0 +1,13 @@
+package com.easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WFKJMYSYSApplication {
+    public static void main(String[] args) {
+        //启动类名.class，启动参数
+        SpringApplication.run(WFKJMYSYSApplication.class, args);
+
+    }
+}
--- src/main/java/com/easy/controller/DepartmentController.java
+++ src/main/java/com/easy/controller/DepartmentController.java
@@ -2,12 +2,19 @@ package com.easy.controller;
 
 import com.easy.bean.Department;
 import com.easy.dao.DepartmentDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class DepartmentController {
-    private DepartmentDao departmentDao;
+    @Autowired
+    DepartmentDao departmentDao;
+    @GetMapping("getdepartmentbyid")
     public Department getDepartmentById(int id) {
-
+   return departmentDao.getDepartmentById(id);
     }
 }
